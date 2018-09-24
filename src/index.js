60338fa6e53f56e554df94e92465c5a7c0e031f8
shaddyshad <mwangishadrack5@gmail.com>
2018-09-24 12:53:38 +0300

    Initialize font awesome library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faShieldAlt, faGem, faBriefcase} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faShieldAlt, faGem, faBriefcase);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
