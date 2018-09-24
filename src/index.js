2de2721c1102e15dfa9a8d6d7da723f0fb3421ba
shaddyshad <mwangishadrack5@gmail.com>
2018-09-24 13:10:50 +0300

    Add few icons

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {faShieldAlt, faGem, faBriefcase} from '@fortawesome/free-solid-svg-icons'
+import {
+    faShieldAlt,
+    faGem,
+    faBriefcase,
+    faUser,
+    faEnvelopeOpenText,
+    faDollarSign,
+    faFile
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faShieldAlt, faGem, faBriefcase);
+library.add(faShieldAlt, faGem, faBriefcase,faUser, faEnvelopeOpenText, faDollarSign, faFile);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
