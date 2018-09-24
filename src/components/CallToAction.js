6d0a8235f90d5509258b8a9a3aaef580a21989cf
shaddyshad <mwangishadrack5@gmail.com>
2018-09-24 13:33:51 +0300

    Render all CTA components

---
+++ src/components/CallToAction.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+//components
+import {
+    Grid, Row, Col, HelpBlock
+} from 'react-bootstrap';
+
+import CTAForm from './CallToActionForm'
+
+const CTA = (props) => {
+    return(
+        <div style={styles.cta}>
+            <Grid>
+                <Row className="show-grid">
+                    <Col xsHidden md={4}>
+                        <h2>Are you crunched for time?</h2>
+                    </Col>
+                    <Col xs={12} md={8}>
+                        <CTAForm/>
+                    </Col>
+                    <HelpBlock>We promise to respond soon.</HelpBlock>
+                </Row>
+            </Grid>
+        </div>
+    );
+};
+
+const styles = {
+    cta: {
+        border: '1px solid black'
+    }
+}
+
+export default CTA;
