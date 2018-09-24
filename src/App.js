6c4b9feabff4cbfb9710daba9a48408afd0ee1de
shaddyshad <mwangishadrack5@gmail.com>
2018-09-24 12:53:24 +0300

    Add values entry

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 //components
 import NavBar from './components/NavBar';
-import Banner from './components/Banner'
+import Banner from './components/Banner';
+import Values from './components/Values';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
           <NavBar/>
           <Banner/>
+          <Values/>
       </div>
     );
   }
