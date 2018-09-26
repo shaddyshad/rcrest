30e0c6f89865c73bc3f3cea22e44909bbe476342
shaddyshad <mwangishadrack5@gmail.com>
2018-09-26 11:20:44 +0300

    Add different routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+    Switch, Route
+} from 'react-router-dom';
+
 //components
-// import LandingPage from './components/Landing';
-// import Orders from './components/Orders';
+import LandingPage from './components/Landing';
+import Orders from './components/Orders';
 import Admin from './components/Admin';
 import User from './components/UserDashboard';
 
@@ -11,10 +15,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {/*<LandingPage/>*/}
-      {/*<Orders/>*/}
-      {/*<Admin notifications={12}/>*/}
-      <User/>
+      <Switch>
+          <Route path='/orders' component={Orders}/>
+          <Route exact path='/sadmin' component={Admin}/>
+          <Route path='/users/:sessid' component={User}/>
+          <Route path='/' component={LandingPage}/>
+      </Switch>
       </div>
     );
   }
