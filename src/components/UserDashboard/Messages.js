4e9cfda50a18a1cb42dd84549088f7c08f0d6107
shaddyshad <mwangishadrack5@gmail.com>
2018-09-27 03:33:19 +0300

    Initialize messaging component

---
+++ src/components/UserDashboard/Messages.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+
+import {
+    Well,
+    HelpBlock,
+    Panel,
+    Button
+} from 'react-bootstrap';
+
+class Notifications extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [
+                {subject: "Easy man",text: "Hey man", from: "Support", to: "Me", time: "Aug 10 17:32:23", order_id: 23}
+            ],
+            shown: false
+        }
+    }
+
+    renderNotification() {
+        const {messages} = this.state;
+        return messages.map(({subject, text, from, order_id, to, time}) => (
+            <Panel onClick={() => {this.setState({shown: !this.state.shown})}}>
+                <Panel.Heading>
+                    <HelpBlock>
+                        <strong>From: {from} -> To: {to} ({time})</strong><span style={{float:'right'}}>ORDER #{order_id}</span>
+                    </HelpBlock>
+                </Panel.Heading>
+                <Panel.Body style={this.state.shown ? {display: 'block'} : {display: 'none'}}>
+                    <HelpBlock><u><b>{subject}</b></u></HelpBlock>
+                    <HelpBlock>{text}</HelpBlock>
+
+                    <Button >Reply</Button>
+                </Panel.Body>
+            </Panel>
+        ));
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Messages</h3>
+                <Panel>
+                    <Panel.Heading>
+                        <Button>Create a message</Button>
+                    </Panel.Heading>
+                    <Panel.Body>
+                        {this.state.messages.length > 0 ? this.renderNotification() :
+                            <HelpBlock>No Messages yet</HelpBlock>}
+                    </Panel.Body>
+                </Panel>
+            </div>
+
+        );
+    }
+}
+
+export default Notifications
