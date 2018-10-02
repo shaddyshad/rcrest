3b5a3f97d39453327dbf2640383fcb41b423b6fd
shaddyshad <mwangishadrack5@gmail.com>
2018-10-02 23:37:44 +0300

    Declare auth user action creators

---
+++ src/actions/authUser.js
@@ -0,0 +1,22 @@
+//Authentication action
+import {RECEIVED_USER, SIGN_OUT} from "../constants/authUser";
+import {init_student} from "./currentOrder/users";
+import {doneLoadingData, loadingData} from "./async";
+import {auth} from "../firebase";
+
+export const receivedUser = authUser => ({type: RECEIVED_USER, payload: authUser});
+
+export const authChanged = authUser => dispatch => {
+    const {uid, email, displayName} = authUser;
+    dispatch(receivedUser({uid, email, displayName, role: 'student'}));
+    dispatch(init_student({uid, email, displayName}));
+};
+
+export const signOut = () => dispatch => {
+    dispatch(loadingData());
+    auth.doSignOut();
+    dispatch(signedOut());
+    dispatch(doneLoadingData())
+};
+
+export const signedOut = () => ({type: SIGN_OUT});
